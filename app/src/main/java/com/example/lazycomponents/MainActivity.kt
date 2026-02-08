404610906741a274f733b2970a76b208da9d2aa8
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2026-02-08 16:01:33 +0100

    fix: evitar solapamiento con barras del sistema

--- app/src/main/java/com/example/lazycomponents/MainActivity.kt
+++ app/src/main/java/com/example/lazycomponents/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import com.example.lazycomponents.nav.AppNavigation
 import com.example.lazycomponents.viewmodel.KaraokeViewModel
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
 
@@ -14,7 +20,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavigation(viewModel = viewModel)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.safeDrawing) // <-- MAGIA
+            ) {
+                AppNavigation(viewModel = viewModel)
+            }
         }
     }
 }
